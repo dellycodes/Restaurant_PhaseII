f592afb0ab3cec5283785fe259efacabbbca9661
Ryan Edelman <ryanEDEL@live.com>
2023-04-26 16:11:18 -0500

    Created the Customer class

---
+++ Restaurant_PhaseII/Model/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_PhaseII.Model
+{
+    public class Customer
+    {
+        private static int autoIncrement;
+        public int ID { get; set; }
+        public string userName {  get; set; }
+        public string password { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public Customer()
+        {
+            autoIncrement++;
+            ID = autoIncrement;
+        }
+    }
+}
