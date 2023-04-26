ce7287e0a4e8abebf3f94e64509b066a457312a8
Ryan Edelman <ryanEDEL@live.com>
2023-04-26 16:30:35 -0500

    Created Reservation class

---
+++ Restaurant_PhaseII/Model/Customers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_PhaseII.Model
+{
+    public class Customers
+    {
+        public List<Customer> customers { get; set; }
+
+        public Customers()
+        {
+            customers = new List<Customer>();
+        }
+
+        public Customer Authenticate(string username, string password)
+        {
+            Customer c = customers.Where(o => o.userName == username).First();
+            if(c != null)
+            {
+                if(c.password == password)
+                {
+                    return c;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            return null;
+        }
+    }
+}
